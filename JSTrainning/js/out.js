0be83451833503c77d64d22afbceb66298b581f7
“youMarishunxiang@gmail.com <mousefly100%>
2016-10-30 13:57:31 +0800

    2016/10/30

---
+++ JSTrainning/js/out.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2016/10/30 0030.
+ */
+
+/**
+ * 打印指定参数数据到页面上并换行。
+ * @param param
+ */
+function println(param) {
+    document.write(param+"<br/>");
+}
+
+/**
+ * 打印指定参数数据到页面上。
+ * @param param
+ */
+function print(param) {
+    document.write(param);
+}
